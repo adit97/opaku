1db2e8e642c2ef4e7ec890e8363ab034bf69141d
adit97 <aditseong97@gmail.com>
2021-06-25 13:53:26 +0700

    rename HomeActivity -> DashboardActivity

--- app/src/main/java/com/opaku/id/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/opaku/id/ui/splashscreen/SplashScreenActivity.kt
@@ -4,13 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.opaku.id.R
-import com.opaku.id.ui.home.HomeActivity
+import com.opaku.id.ui.home.DashboardActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        startActivity(Intent(this, HomeActivity::class.java))
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
