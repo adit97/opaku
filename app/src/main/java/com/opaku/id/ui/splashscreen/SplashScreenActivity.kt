02867bebdc4c1d4f1f2862590a80f7519f185208
adit97 <aditseong97@gmail.com>
2021-06-25 13:49:25 +0700

    update SplashScreenActivity theme

---
+++ app/src/main/java/com/opaku/id/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.opaku.id.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.opaku.id.R
+import com.opaku.id.ui.home.HomeActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        startActivity(Intent(this, HomeActivity::class.java))
+    }
+}
